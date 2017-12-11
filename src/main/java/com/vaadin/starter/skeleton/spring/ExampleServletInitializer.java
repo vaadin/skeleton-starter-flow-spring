30afbb0c038546ac63b4d84ec7115a90934b21da
Artur <artur@vaadin.com>
2017-12-11 22:32:07 +0200

    Use Spring 2.0.0.M7 as defined by Vaadin alpha9 (#25)

--- src/main/java/com/vaadin/starter/skeleton/spring/ExampleServletInitializer.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/ExampleServletInitializer.java
@@ -17,7 +17,7 @@ package com.vaadin.starter.skeleton.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
