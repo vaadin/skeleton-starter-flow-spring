8f50fa482b1209ac33e115218117e5df88c2aa01
Pekka Hyvönen <pekka@vaadin.com>
2018-02-02 19:03:36 +0200

    Apply Best Practices, change license

--- src/main/java/com/vaadin/starter/skeleton/spring/ExampleTemplate.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/ExampleTemplate.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2000-2017 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.vaadin.starter.skeleton.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +14,7 @@ import com.vaadin.flow.templatemodel.TemplateModel;
  * Simple template example.
  */
 @Tag("example-template")
-@HtmlImport("frontend://ExampleTemplate.html")
+@HtmlImport("src/example-template.html")
 @SpringComponent
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ExampleTemplate extends PolymerTemplate<ExampleTemplate.ExampleModel> {
