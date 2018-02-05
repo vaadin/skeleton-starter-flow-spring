9bd6125123a8d56589fad37cf057ac53b42d96c8
Pekka Hyvönen <pekka@vaadin.com>
2018-02-05 15:02:01 +0200

    Apply Best Practices, change license (#40)

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -1,41 +1,21 @@
-/*
- * Copyright 2000-2017 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.vaadin.starter.skeleton.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * The main view contains a simple label element and a template element.
  */
-@BodySize(height = "100vh", width = "100vw")
-@HtmlImport("styles.html")
+@HtmlImport("styles/shared-styles.html")
 @Route("")
 @Theme(Lumo.class)
+@BodySize(height = "100vh", width = "100vw")
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired ExampleTemplate template) {
@@ -43,6 +23,7 @@ public class MainView extends VerticalLayout {
         Button button = new Button("Click me", event -> template.setValue("Clicked!"));
         // This is a simple template example
         add(button, template);
+        setClassName("main-layout");
     }
 
 }
