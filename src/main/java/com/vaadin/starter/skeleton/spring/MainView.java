c2e872b77e00e4b0e949ca3a2b5304ca755ee160
Gilberto Torrezan <gilberto@vaadin.com>
2018-04-04 15:19:28 +0300

    Removed the @Theme declaration

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -1,26 +1,25 @@
 package com.vaadin.starter.skeleton.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * The main view contains a simple label element and a template element.
  */
 @HtmlImport("styles/shared-styles.html")
 @Route("")
-@Theme(Lumo.class)
 @BodySize(height = "100vh", width = "100vw")
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired ExampleTemplate template) {
         // This is just a simple label created via Elements API
-        Button button = new Button("Click me", event -> template.setValue("Clicked!"));
+        Button button = new Button("Click me",
+                event -> template.setValue("Clicked!"));
         // This is a simple template example
         add(button, template);
         setClassName("main-layout");
