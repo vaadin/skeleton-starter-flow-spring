7a93c33027b6c34d4455f4d13dee0c37a749a59f
Pekka Maanpää <pekkamaa@vaadin.com>
2018-04-04 16:38:33 +0300

    Remove also the import

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -3,7 +3,6 @@ package com.vaadin.starter.skeleton.spring;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
