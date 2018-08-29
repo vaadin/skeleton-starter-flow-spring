31d301183c7e1a61b852e75f8b3a02304ab432f6
Matti Tahvonen <matti@vaadin.com>
2018-08-29 09:19:00 +0300

    Simplified the simple version of Spring Boot Starter (#88)
    * Simplified the simple version of Spring Boot Starter
    * Updated readme to help people use Spring Boot like it is designed

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -1,26 +1,19 @@
 package com.vaadin.starter.skeleton.spring;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-/**
- * The main view contains a simple label element and a template element.
- */
-@HtmlImport("styles/shared-styles.html")
 @Route
 public class MainView extends VerticalLayout {
 
-    public MainView(@Autowired ExampleTemplate template) {
-        // This is just a simple label created via Elements API
+    public MainView(@Autowired MessageBean bean) {
         Button button = new Button("Click me",
-                event -> template.setValue("Clicked!"));
-        // This is a simple template example
-        add(button, template);
-        setClassName("main-layout");
+                e -> Notification.show(bean.getMessage()));
+        add(button);
     }
 
 }
