eb5390f887e5ba28d95ab6f22045a4fdca36eae3
Mikko Salminen <mikko@vaadin.com>
2018-09-19 14:17:21 +0300

    Added pwa support. (#91)
    - Added @PWA -annotation.
    - Added icon.

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
 
 @Route
+@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired MessageBean bean) {
