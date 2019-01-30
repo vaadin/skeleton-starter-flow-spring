2d0630760105f68948599fdbaa532411cb48d673
Denis <denis@vaadin.com>
2019-01-30 10:38:59 +0300

    Make application extend SpringBootServletInitializer (#119)
    Fixes #116

--- src/main/java/com/vaadin/starter/skeleton/spring/Application.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/Application.java
@@ -2,12 +2,13 @@ package com.vaadin.starter.skeleton.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
