531c11b3ddc08634abae5481e5bb449d554174bb
Manolo Carrasco <manolo@apache.org>
2019-10-02 08:21:26 +0200

    CCDM: use serverSiceRoutes API, and latest versions

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,14 +1,14 @@
 import {Flow} from '@vaadin/flow-frontend/Flow';
 import {Router} from '@vaadin/router';
 
-const flow = new Flow({
+const {serverSideRoutes} = new Flow({
   // @ts-ignore
   imports: () => import('../target/frontend/generated-flow-imports')
 });
 
 const routes = [
   // fallback to server-side Flow routes if no client-side routes match
-  flow.route
+  ...serverSideRoutes
 ];
 
 const router = new Router(document.querySelector('#outlet'));
