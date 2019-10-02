2fa26484340b84b0d80eb21bf9c9cc20cf081ebf
Tien Nguyen <nqtien.dev@gmail.com>
2019-10-02 23:34:25 +0300

    Fix routes

--- frontend/index.ts
+++ frontend/index.ts
@@ -6,5 +6,10 @@ const {serverSideRoutes} = new Flow({
   imports: () => import('../target/frontend/generated-flow-imports')
 });
 
+const routes = [
+  // fallback to server-side Flow routes if no client-side routes match
+  ...serverSideRoutes
+];
+
 const router = new Router(document.querySelector('#outlet'));
-router.setRoutes(serverSideRoutes);
+router.setRoutes(routes);
