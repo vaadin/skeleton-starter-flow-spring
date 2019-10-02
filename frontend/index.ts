ebed793f0ee0d81f36af09109c3639f28134eafa
Tien Nguyen <nqtien.dev@gmail.com>
2019-10-02 23:18:11 +0300

    Use v15 snapshot

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,15 +1,10 @@
 import {Flow} from '@vaadin/flow-frontend/Flow';
 import {Router} from '@vaadin/router';
 
-const flow = new Flow({
+const {serverSideRoutes} = new Flow({
   // @ts-ignore
   imports: () => import('../target/frontend/generated-flow-imports')
 });
 
-const routes = [
-  // fallback to server-side Flow routes if no client-side routes match
-  flow.route
-];
-
 const router = new Router(document.querySelector('#outlet'));
-router.setRoutes(routes);
+router.setRoutes(serverSideRoutes);
