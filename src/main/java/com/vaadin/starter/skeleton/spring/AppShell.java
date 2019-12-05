66255f3086b12a420d8e501782d3d95d28674575
Viktor Lukashov <viktor@vaadin.com>
2019-12-05 11:15:32 +0200

    update to Vaadin 15.0.0.alpha6
     - Spring Boot 2.2.0
     - Move the @PWA annotation to a new AppShell class

---
+++ src/main/java/com/vaadin/starter/skeleton/spring/AppShell.java
@@ -0,0 +1,8 @@
+package com.vaadin.starter.skeleton.spring;
+
+import com.vaadin.flow.component.page.VaadinAppShell;
+import com.vaadin.flow.server.PWA;
+
+@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+public class AppShell extends VaadinAppShell {
+}
