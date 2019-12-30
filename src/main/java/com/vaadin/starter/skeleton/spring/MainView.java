76ac954fcdf30392b44b2d211f1d26d74cee455a
Sami Ekblad <sami@vaadin.com>
2019-12-30 13:45:57 +0200

    Added code comments.

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -9,6 +9,14 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
+/** A sample Vaadin view class.
+ *
+ *  To implement a Vaadin view just extend any Vaadin component and
+ *  use @Route annotation to announce it in a URL as a Spring managed bean.
+ *  Use the @PWA annotation make the application installable on mobile
+ *  phone or desktop.
+ *  New instance of this class is created for every new user.
+ */
 @Route
 @PWA(name = "Vaadin Application",
         shortName = "Vaadin App",
@@ -17,6 +25,12 @@ import com.vaadin.flow.server.PWA;
 @CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
 
+    /** Construct new Vaadin view.
+     *
+     * Build the initial UI state for the user accessing the application.
+     *
+     * @param service The message service. Automatically injected Spring managed bean.
+     */
     public MainView(@Autowired MessageService service) {
         Button button = new Button("Click me",
                 e -> Notification.show(service.getMessage()));
