2908bb7f9c586b4156ef67a6a703501f6bf901a8
Sami Ekblad <sami@vaadin.com>
2019-12-30 13:43:50 +0200

    Added simple CSS example

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -1,6 +1,7 @@
 package com.vaadin.starter.skeleton.spring;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,11 +11,13 @@ import com.vaadin.flow.server.PWA;
 
 @Route
 @PWA(name = "Vaadin App", shortName = "Vaadin App")
+@CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired MessageService service) {
         Button button = new Button("Click me",
                 e -> Notification.show(service.getMessage()));
+        button.setClassName("my-style");
         add(button);
     }
 
