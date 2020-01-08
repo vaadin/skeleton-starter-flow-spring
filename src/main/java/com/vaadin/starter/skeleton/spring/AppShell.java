fdb9485fc5bb5b6f1ce48653827728b9572c2282
haijian <haijian@vaadin.com>
2020-01-08 09:29:49 +0200

    Revert "Update to use AppShellConfigurator (#276)"
    This reverts commit 82fbd955af8b76779a799b17e4cfb0a47eba5e55.

--- src/main/java/com/vaadin/starter/skeleton/spring/AppShell.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/AppShell.java
@@ -1,8 +1,8 @@
 package com.vaadin.starter.skeleton.spring;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.VaadinAppShell;
 import com.vaadin.flow.server.PWA;
 
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
-public class AppShell implements AppShellConfigurator {
+public class AppShell extends VaadinAppShell {
 }
