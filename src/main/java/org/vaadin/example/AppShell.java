2748c9fddcc3cf1751119eb00e63944db2793cb2
Anastasia Smirnova <anastsmirnova96@gmail.com>
2020-06-16 10:58:38 +0300

    Remove deprecated and discarded feature

--- src/main/java/org/vaadin/example/AppShell.java
+++ src/main/java/org/vaadin/example/AppShell.java
@@ -7,6 +7,6 @@ import com.vaadin.flow.server.PWA;
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
-@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base", enableInstallPrompt = false)
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
 public class AppShell implements AppShellConfigurator {
 }
