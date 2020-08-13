e499bc1f67ceecb358642ec93a2a8cf32143ddb0
Artur Signell <artur@vaadin.com>
2020-08-13 11:46:06 +0300

    Automatically launch a browser when running in development mode

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -3,6 +3,8 @@ package org.vaadin.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 /**
  * The entry point of the Spring Boot application.
@@ -11,7 +13,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext app = SpringApplication.run(Application.class, args);
+        LaunchUtil.launchBrowserInDevelopmentMode(app);
     }
 
 }
