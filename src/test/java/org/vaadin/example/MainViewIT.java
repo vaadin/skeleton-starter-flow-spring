699b05dbf79c10ae3cdee0076b494a9af68b3ccf
Mikhail Shabarov <mikhail@vaadin.com>
2021-08-20 18:19:23 +0300

    test: Wait for elements to be present before asserts

--- src/test/java/org/vaadin/example/MainViewIT.java
+++ src/test/java/org/vaadin/example/MainViewIT.java
@@ -15,29 +15,31 @@ public class MainViewIT extends AbstractViewTest {
     @Test
     public void clickingButtonShowsNotification() {
         Assert.assertFalse($(NotificationElement.class).exists());
-        $(ButtonElement.class).first().click();
+        $(ButtonElement.class).waitForFirst().click();
         Assert.assertTrue($(NotificationElement.class).waitForFirst().isOpen());
     }
 
     @Test
     public void clickingButtonTwiceShowsTwoNotifications() {
         Assert.assertFalse($(NotificationElement.class).exists());
-        ButtonElement button = $(ButtonElement.class).first();
+        ButtonElement button = $(ButtonElement.class).waitForFirst();
         button.click();
         button.click();
+        $(NotificationElement.class).waitForFirst();
         Assert.assertEquals(2, $(NotificationElement.class).all().size());
     }
 
     @Test
     public void buttonIsUsingLumoTheme() {
-        WebElement element = $(ButtonElement.class).first();
+        WebElement element = $(ButtonElement.class).waitForFirst();
         assertThemePresentOnElement(element, Lumo.class);
     }
 
     @Test
     public void testClickButtonShowsHelloAnonymousUserNotificationWhenUserNameIsEmpty() {
-        ButtonElement button = $(ButtonElement.class).first();
+        ButtonElement button = $(ButtonElement.class).waitForFirst();
         button.click();
+        $(NotificationElement.class).waitForFirst();
         Assert.assertTrue($(NotificationElement.class).exists());
         NotificationElement notification = $(NotificationElement.class).first();
         Assert.assertEquals("Hello anonymous user", notification.getText());
@@ -45,10 +47,10 @@ public class MainViewIT extends AbstractViewTest {
 
     @Test
     public void testClickButtonShowsHelloUserNotificationWhenUserIsNotEmpty() {
-        TextFieldElement textField = $(TextFieldElement.class).first();
+        TextFieldElement textField = $(TextFieldElement.class).waitForFirst();
         textField.setValue("Vaadiner");
-        ButtonElement button = $(ButtonElement.class).first();
-        button.click();
+        $(ButtonElement.class).waitForFirst().click();
+        $(NotificationElement.class).waitForFirst();
         Assert.assertTrue($(NotificationElement.class).exists());
         NotificationElement notification = $(NotificationElement.class).first();
         Assert.assertEquals("Hello Vaadiner", notification.getText());
@@ -56,9 +58,10 @@ public class MainViewIT extends AbstractViewTest {
 
     @Test
     public void testEnterShowsHelloUserNotificationWhenUserIsNotEmpty() {
-        TextFieldElement textField = $(TextFieldElement.class).first();
+        TextFieldElement textField = $(TextFieldElement.class).waitForFirst();
         textField.setValue("Vaadiner");
         textField.sendKeys(Keys.ENTER);
+        $(NotificationElement.class).waitForFirst();
         Assert.assertTrue($(NotificationElement.class).exists());
         NotificationElement notification = $(NotificationElement.class).first();
         Assert.assertEquals("Hello Vaadiner", notification.getText());
