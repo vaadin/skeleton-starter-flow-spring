de200e6dbd8847e99f1df83435db063a56cdb06c
Artur Signell <artur@vaadin.com>
2022-12-15 11:00:55 +0200

    Revert app class to avoid duplicate annotations in generated project

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -1,12 +1,11 @@
 package org.vaadin.example;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 
 /**
  * The entry point of the Spring Boot application.
@@ -16,9 +15,7 @@ import com.vaadin.flow.theme.Theme;
  *
  */
 @SpringBootApplication
-@NpmPackage(value = "line-awesome", version = "1.3.0")
-@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
-@Theme("myapp")
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
