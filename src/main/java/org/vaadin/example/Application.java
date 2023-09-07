493c5a342814bc286c90c819f34b30986a8ccbd9
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2023-09-07 16:50:14 +0300

    chore: Add custom theme (#1066)

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -2,6 +2,8 @@ package org.vaadin.example;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
+@Theme("my-theme")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
