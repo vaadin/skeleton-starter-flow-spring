e3f12463605d1cfc7f7bf07d1ad59dfe4019b3b5
Mikhail Shabarov <mikhail@vaadin.com>
2023-10-12 17:25:58 +0300

    Only change eager mode to true

--- src/main/java/org/vaadin/example/SecondaryView.java
+++ src/main/java/org/vaadin/example/SecondaryView.java
@@ -2,8 +2,10 @@ package org.vaadin.example;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 
 import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.UI;
@@ -14,6 +16,7 @@ import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinRequest;
 
 @PageTitle("Secondary View")
 @Route(value = "secondary")
@@ -21,35 +24,32 @@ public class SecondaryView extends VerticalLayout implements AfterNavigationObse
 
     Div cookieContainer = new Div();
     Div noPathCookieContainer = new Div();
+    Div pathInfoContainer = new Div();
 
     public SecondaryView() {
         setSpacing(false);
 
         var text = new Span("Current cookie value:");
         var noPathText = new Span("Current no path cookie value:");
-        add(text, cookieContainer, noPathText, noPathCookieContainer);
+        var pathInfo = new Span("Path Info:");
+        add(text, cookieContainer, noPathText, noPathCookieContainer, pathInfo, pathInfoContainer);
     }
 
     @Override
     public void afterNavigation(AfterNavigationEvent event) {
         var cookie = getCookieValue("SecondaryView");
         var noPathCookie = getCookieValue("SecondaryViewNoPath");
+        System.out.println("SecondaryView cookie: " + cookie + ", SecondaryViewNoPath:"+ noPathCookie);
         cookieContainer.setText(cookie);
         noPathCookieContainer.setText(noPathCookie);
+        pathInfoContainer.setText(VaadinRequest.getCurrent().getPathInfo());
     }
 
-    public String getCookieValue(String key) {
-        Optional<UI> ui = this.getUI();
-        if (ui.isPresent()) {
-            Cookie[] cookies = (Cookie[]) ComponentUtil.getData(ui.get(), "cookies");
-            if (cookies == null) {
-                return null;
-            }
-            return Arrays.stream(cookies).filter(cookie -> key.equals(cookie.getName())).findFirst().map(
-                    Cookie::getValue
-            ).orElse(null);
-        } else {
-            return null;
-        }
+    public static String getCookieValue(String key) {
+        var request = (HttpServletRequest) VaadinRequest.getCurrent();
+
+        return Optional.ofNullable(request.getCookies()).stream().flatMap(Stream::of)
+                .filter(cookie -> key.equals(cookie.getName()))
+                .findFirst().map(Cookie::getValue).orElse(null);
     }
 }
