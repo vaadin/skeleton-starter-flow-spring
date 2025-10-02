3a92e3d3bcf62f558bb3f63958d167bcc7c224a6
Artur <artur@vaadin.com>
2025-10-02 13:22:01 +0300

    feat: Use Aura and @StyleSheet (#1387)

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -1,10 +1,11 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 
 /**
  * The entry point of the Spring Boot application.
@@ -15,7 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
-@Theme("my-theme")
+@StyleSheet("styles.css")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
